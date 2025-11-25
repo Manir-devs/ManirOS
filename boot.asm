66f006408085185e5bf3ff52a4e658419cd55148
Manir-devs <maniruz.zaman.primary@gmail.com>
2025-11-26 04:19:47 +0530

    Updated boot.asm

--- boot.asm
+++ boot.asm
@@ -1,1 +1,16 @@
 org 0x7C00
+start:
+    mov si, message
+loop:
+    lodsb
+    cmp al, 0
+    je kill
+    mov ah, 0x0E
+    int 0x10
+    jmp loop
+kill:
+    jmp kill
+message:
+    db "Hello World", 0
+times 510-($-$$) db 0
+dw 0xAA55
